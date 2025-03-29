9094152d04a514ae77c84f3ed7a64ac0e2796857
LaszloR1 <laszlo.csoma@pm.me>
2025-03-29 20:40:35 +0000

    initial commit

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,25 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    version = "*",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+    },
+    cmd = "Neotree",
+    keys = {
+        { "<leader>nt", ":Neotree reveal<CR>", { desc = "NeoTree reveal" } },
+    },
+    opts = {
+        filesystem = {
+            filtered_items = {
+                visible = true,
+            },
+            window = {
+                mappings = {
+                    ["\\"] = "close_window",
+                },
+            },
+        },
+    },
+}
---
+++ lua/vim-options.lua
@@ -0,0 +1,87 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.g.have_nerd_font = true
+
+vim.opt.swapfile = false
+
+-- Set netrw disabled
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.mouse = "a"
+
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = false
+
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = true
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+    vim.opt.clipboard = "unnamedplus"
+end)
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = "yes"
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+-- vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+vim.opt.scrolloff = 10
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        local args = vim.fn.argv()
+        if #args == 1 and vim.fn.isdirectory(args[1]) == 1 then
+            require("neo-tree.command").execute({ dir = args[1] })
+        end
+    end,
+})
