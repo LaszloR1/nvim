3044d70068611c0674c920c3588f0ff111d79306
LaszloR1 <laszlo.csoma@pm.me>
2025-04-05 17:34:23 +0000

    added go formatter

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -11,6 +11,7 @@ return {
                 null_ls.builtins.diagnostics.rubocop,
                 null_ls.builtins.formatting.rubocop,
                 null_ls.builtins.formatting.alejandra,
+                null_ls.builtins.formatting.gofmt,
             },
             on_attach = function(client, bufnr)
                 if client.supports_method("textDocument/formatting") then
