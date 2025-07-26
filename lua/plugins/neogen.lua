7073635c99addec9988265cc488ab0cf22e71c9b
LaszloR1 <laszlo.csoma@pm.me>
2025-07-26 21:02:18 +0000

    plugin for generating docs (jsdoc)

---
+++ lua/plugins/neogen.lua
@@ -0,0 +1,9 @@
+return {
+    "danymat/neogen",
+    config = true,
+    -- Uncomment next line if you want to follow only stable versions
+    -- version = "*"
+    keys = {
+        { "<leader>ng", ":Neogen<CR>", desc = "Neogen generate" },
+    },
+}
